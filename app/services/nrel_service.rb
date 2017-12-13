520ae33694e34ba1281ec8a14e12f10c5444229b
Liam Barstad <liambarstad@gmail.com>
2017-12-13 09:47:59 -0700

    wip nrel service

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,17 +1,29 @@
 class NRELService
 
   def initialize(params)
-    @zip_code = params[:zip_code]
+    @location = params[:zip_code]
   end
 
   def get_stations
+    station_connection.get('/nearest') do |f|
+      set_params(f)
+    end
 
   end
 
   private
 
-    def connection
+    def station_connection
+      Faraday.new('/api/alt-fuel-stations/v1') do |f|
+        f.params['format'] = 'json'
+        f.params['api_key'] = ENV['NREL_API_KEY']
+      end
+    end
 
+    def set_params(faraday_object)
+      instance_values.each do |name, value|
+        faraday_object.params["#{name}"] = value
+      end
     end
 
 end
