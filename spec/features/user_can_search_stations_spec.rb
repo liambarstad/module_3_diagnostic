315b84c9565729b3c764fe589d787de3371dd9b8
Liam Barstad <liambarstad@gmail.com>
2017-12-13 09:24:19 -0700

    add searchbar

--- spec/features/user_can_search_stations_spec.rb
+++ spec/features/user_can_search_stations_spec.rb
@@ -1,10 +1,3 @@
-And I fill in the search form with 80203
-And I click "Locate"
-Then I should be on page "/search" with parameters visible in the url
-Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-And the stations should be limited to Electric and Propane
-And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
-
 require 'rails_helper'
 
 RSpec.feature "user can search stations" do
@@ -20,5 +13,7 @@ RSpec.feature "user can search stations" do
     expect(page).to have_css('.station-fuel-type', count: 10)
     expect(page).to have_css('.station-distance', count: 10)
     expect(page).to have_css('.station-access-times', count: 10)
+    expect(page).to_not have_css('.station-fuel-type', text: '') #stations other than electric/propane
+    expect(page).to_not have_css('.station-fuel-type', text: '')
   end
 end
